0c30cc504286666efb6828f857bd3298e266617b
Veka741 <164997511+Veka741@users.noreply.github.com>
2025-05-22 08:14:47 +0200

    index file

---
+++ app/Views/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=" <?= base_url('node_modules/bootstrap/dist/css/bootstrap.min.css') ?>">
+    <title>Závody</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1> La Tropicale Amissa Bongo </h1>
+        <?php
+            $table = new \CodeIgniter\View\Table();
+            $table->setHeading('Ročník', "Délka", "Začátek", "Konec" );
+            
+            foreach($race_year as $row) {
+                $table->addRow($row->year, ".", $row->start_date, $row->end_date );
+            }
+
+            $template = array(
+                'table_open'=> '<table class="table table-bordered">',
+                'thead_open'=> '<thead>',
+                'thead_close'=> '</thead>',
+                'heading_row_start'=> '<tr>',
+                'heading_row_end'=>' </tr>',
+                'heading_cell_start'=> '<th>',
+                'heading_cell_end' => '</th>',
+                'tbody_open' => '<tbody>',
+                'tbody_close' => '</tbody>',
+                'row_start' => '<tr>',
+                'row_end'  => '</tr>',
+                'cell_start' => '<td>',
+                'cell_end' => '</td>',
+                'row_alt_start' => '<tr>',
+                'row_alt_end' => '</tr>',
+                'cell_alt_start' => '<td>',
+                'cell_alt_end' => '</td>',
+                'table_close' => '</table>'
+                );
+                
+                $table->setTemplate($template);
+
+            echo $table->generate();
+        ?>
+    </div>
+</body>
+</html>

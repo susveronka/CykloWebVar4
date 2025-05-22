bd8de747f61d797dab356605e1e0352228536e3f
susveronka <sustkova_veronika@oauh.cz>
2025-05-22 07:49:34 +0200

    .

---
+++ app/Controllers/Main.php
@@ -0,0 +1,20 @@
+
+Vypište všechny ročníky závodu La Tropicale Amissa Bongo(id race=83), které máme v databázi. 
+U každého ročníku bude uvedena celková délka závodu (součet délek všech etap), datum začátku a konce.
+ a následně odkaz na jednotlivé  etapy, kam vložíte start a cíl etapy, datum etapy, délku etapy a výsledky prvních deseti závodníků  etapě.
+
+Vytvoříte rovněž formulář, který umožní editovat výsledky etapy
+ - bude možno měnit jméno a příjmení závodníka, čas závodníka v etapě a pořadí v etapě (to poslední dropdownem)
+
+
+1 - index.php - tabůlka -  s roky závodu u nich délka závodu (součet všech etap), datum začátku a konce
+2 rozklik na na seznam etap vybraného roku
+3 jednotlivá etapa 
+-  start etapy
+-  cíl etapy
+-  datum etapy, délku etapy s výsledek prvních 10 závodníků
+
+4 - formulář na editování výsledku etapy  -změny
+ -  jména
+-  příjmení
+-  čas a pořadí v etapě

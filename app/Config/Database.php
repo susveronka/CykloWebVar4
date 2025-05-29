a44115edb8fbef647d276b50cd644cce78cb9f3e
susveronka <sustkova_veronika@oauh.cz>
2025-05-29 07:40:54 +0200

    .

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'stranakova',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => 'c_',
         'pConnect'     => false,

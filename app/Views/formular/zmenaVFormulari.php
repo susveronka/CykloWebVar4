f98329766d669a8c7c8f8cd561edf123efdd3518
susveronka <sustkova_veronika@oauh.cz>
2025-06-05 07:44:42 +0200

    .

--- app/Views/formular/zmenaVFormulari.php
+++ app/Views/formular/zmenaVFormulari.php
@@ -2,12 +2,7 @@
 // zmenaVFormulari.php
 
 // Předpokládáme, že data závodníka jsou načtena z databáze
-$zavodnik = [
-    'jmeno' => 'Jan',
-    'prijmeni' => 'Novak',
-    'cas' => '01:30:00',
-    'poradi' => 1
-];
+
 
 // Možnosti pro pořadí
 $poradiMoznosti = range(1, 10); // Příklad pro 10 závodníků
@@ -16,23 +11,21 @@ $poradiMoznosti = range(1, 10); // Příklad pro 10 závodníků
 
     <h1>Úprava výsledků etapy</h1>
     <form action="zpracovani.php" method="post">
-        <label for="jmeno">Jméno:</label>
-        <input type="text" id="jmeno" name="jmeno" value="<?php echo htmlspecialchars($zavodnik['jmeno']); ?>" required><br>
 
-        <label for="prijmeni">Příjmení:</label>
-        <input type="text" id="prijmeni" name="prijmeni" value="<?php echo htmlspecialchars($zavodnik['prijmeni']); ?>" required><br>
-
-        <label for="cas">Čas:</label>
-        <input type="time" id="cas" name="cas" value="<?php echo htmlspecialchars($zavodnik['cas']); ?>" required><br>
-
-        <label for="poradi">Pořadí:</label>
-        <select id="poradi" name="poradi" required>
+        <label for="rank">Pořadí:</label>
+        <select id="rank" name="rank" required>
             <?php foreach ($poradiMoznosti as $zmenaPoradi): ?>
-                <option value="<?php echo $zmenaPoradi; ?>" <?php echo $zmenaPoradi == $zavodnik['poradi'] ? 'selected' : ''; ?>>
+                <option value="<?php echo $zmenaPoradi; ?>" <?php echo $zmenaPoradi == $zavodnik['rank'] ? 'selected' : ''; ?>>
                     <?php echo $zmenaPoradi; ?>
                 </option>
             <?php endforeach; ?>
         </select><br>
+
+        <label for="name_link">Jméno:</label>
+        <input type="text" id="name_link" name="name_link" value="<?php echo htmlspecialchars($zavodnik['name_link']); ?>" required><br>
+
+        <label for="cas">Čas:</label>
+        <input type="time" id="cas" name="cas" value="<?php echo htmlspecialchars($zavodnik['time']); ?>" required><br>
 
         <input type="submit" value="Uložit změny">
     </form>

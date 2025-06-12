28ff5a166b68853ac1ef84515e2e2cd699819719
Veka741 <164997511+Veka741@users.noreply.github.com>
2025-06-12 07:43:09 +0200

    .

--- app/Views/formular/zmenaVFormulari.php
+++ app/Views/formular/zmenaVFormulari.php
@@ -4,22 +4,20 @@
 
 ?>
 
-    <h1>Úprava výsledků etapy</h1>
-    <form action="zmena" method="post">
+<h1>Úprava výsledků etapy</h1>
+<form action="zmena" method="post">
 
     <label for="rank">Pořadí:</label>
-    <input type="text" id="rank" name="rank" value="<?php echo htmlspecialchars($rider['rank']); ?>" required><br>
+    <input type="text" id="rank" name="rank" value="<?php echo htmlspecialchars($rider['rank'] ?? ''); ?>" required><br>
 
+    <label for="first_name">Jméno:</label>
+    <input type="text" id="first_name" name="first_name" value="<?php echo htmlspecialchars($rider['first_name'] ?? ''); ?>" required><br>
 
-        <label for="first_name">Jméno:</label>
-        <input type="text" id="first_name" name="first_name" value="<?php echo htmlspecialchars($rider['first_name']); ?>" required><br>
+    <label for="last_name">Příjmení:</label>
+    <input type="text" id="last_name" name="last_name" value="<?php echo htmlspecialchars($rider['last_name'] ?? ''); ?>" required><br>
 
-        <label for="last_name">Příjmení:</label>
-        <input type="text" id="last_name" name="last_name" value="<?php echo htmlspecialchars($rider['last_name']); ?>" required><br>
+    <label for="cas">Čas:</label>
+    <input type="time" id="cas" name="cas" value="<?php echo htmlspecialchars($rider['time'] ?? ''); ?>" required><br>
 
-
-        <label for="cas">Čas:</label>
-        <input type="time" id="cas" name="cas" value="<?php echo htmlspecialchars($rider['time']); ?>" required><br>
-
-        <input type="submit" value="Uložit změny">
-    </form>
+    <input type="submit" value="Uložit změny">
+</form>

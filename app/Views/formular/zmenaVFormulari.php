7a783712e4aa0c198dc12434d71798dbaa954247
susveronka <sustkova_veronika@oauh.cz>
2025-06-12 07:53:25 +0200

    .

--- app/Views/formular/zmenaVFormulari.php
+++ app/Views/formular/zmenaVFormulari.php
@@ -22,7 +22,6 @@
     <label for="last_name">Příjmení:</label>
     <input type="text" id="last_name" name="last_name" value="<?php echo htmlspecialchars($rider['last_name'] ?? ''); ?>" required><br>
 
-
     <label for="cas">Čas:</label>
     <input type="time" id="cas" name="cas" value="<?php echo htmlspecialchars($rider['time'] ?? ''); ?>" required><br>
 
